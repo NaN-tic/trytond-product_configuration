617f015e03d5eeceb255bcf5f18c608f878f0bcf
Guillem Barba <guillem@nan-tic.com>
2014-07-22 16:05:40 +0200

    [#020748] Add infraestructure to get fields depending on company in product.template and product.category

--- __init__.py
+++ __init__.py
@@ -1,10 +1,16 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
+from .helper import *
 from .configuration import *
+from .product import *
 
 
 def register():
     Pool.register(
         Configuration,
+        CategoryCompany,
+        Category,
+        TemplateCompany,
+        Template,
         module='product_configuration', type_='model')
